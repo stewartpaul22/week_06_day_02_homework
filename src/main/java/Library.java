397fb732197e7e13fede4b73ee1ad1bddc63bd2f
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-02-27 19:50:48 +0000

    Initial commit.

---
+++ src/main/java/Library.java
@@ -0,0 +1,27 @@
+import java.awt.print.Book;
+import java.util.ArrayList;
+
+public class Library {
+
+    private ArrayList<Book> stock;
+    private int capacity;
+
+    public ArrayList<Book> getStock() {
+        return stock;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+
+
+
+
+
+
+}
